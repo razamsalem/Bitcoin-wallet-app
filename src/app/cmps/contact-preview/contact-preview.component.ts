556c312b5ebfcbce7152dc4105f6b7013659716c
Raz <amsalemraz10@gmail.com>
2023-11-24 20:18:11 +0200

    /F/ContactPreview/Actions buttons works properly

--- src/app/cmps/contact-preview/contact-preview.component.ts
+++ src/app/cmps/contact-preview/contact-preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../../models/contact.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-preview',
@@ -9,9 +10,16 @@ import { Contact } from '../../models/contact.model';
 export class ContactPreviewComponent {
   @Input() contact!: Contact
   @Output() remove = new EventEmitter()
+  constructor(private router: Router) {} 
 
-  onRemoveContact() {
+  onRemoveContact(event: Event) {
+    event.stopPropagation()
     this.remove.emit(this.contact._id)
+  }
+
+  onEditContact(event: Event) {
+    event.stopPropagation()
+    this.router.navigate(['/contact/edit', this.contact._id])
   }
 
 }
